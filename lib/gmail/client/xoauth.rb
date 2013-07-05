db98727054b28481f728f06baf43121f250c159f
Peter Lin <ptrln@me.com>
2013-07-04 17:12:41 -0700

    fixed oauth2 token error

--- lib/gmail/client/xoauth.rb
+++ lib/gmail/client/xoauth.rb
@@ -4,16 +4,9 @@ module Gmail
   module Client
     class XOAuth < Base
       attr_reader :token
-      attr_reader :secret
-      attr_reader :consumer_key
-      attr_reader :consumer_secret
 
       def initialize(username, options={})
         @token           = options.delete(:token)
-        @secret          = options.delete(:secret)
-        @consumer_key    = options.delete(:consumer_key)
-        @consumer_secret = options.delete(:consumer_secret)
-       
         super(username, options)
       end
 
